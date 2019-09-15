b9e9145f128ebc28bf40ec7e15f7d0703e4b2af9
Ethans Institute <erjitpan@gmail.com>
2019-09-15 12:19:32 +0530

    Divide Function

--- calc.py
+++ calc.py
@@ -4,10 +4,10 @@ def subtract(x,y):
     pass
 def multiply(x,y):
     pass
-def divide(x,y):
-    pass
 def square(x,y):
     pass
 def multiply(x,y):
     return x*y
+def divide(x,y):
+    return x/y
 
